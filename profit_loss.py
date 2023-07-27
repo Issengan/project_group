31fd3ad3a3c1465081159a871d60db0d22c908f2
avrialim <139242319+avrialim@users.noreply.github.com>
2023-07-27 23:07:45 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,1 +1,9 @@
-Xuan Ting
+#Import the csv module
+import csv
+#Function to read CSV file and return data as a list of dictionaries
+def read_csv_file(file_path):
+  """
+  Read a CSV file and return the data as a list of dictionaries.
+  Parameters:
+  file_path(str):
+  
