0d6ae75545e68b6051b70793289ca48c61183e76
guishant <140525028+guishant@users.noreply.github.com>
2023-07-28 03:11:12 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,1 +1,15 @@
-Gui Shan
+import csv 
+
+def read_csv_file(file_path):
+
+    data = [] 
+
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        headers = next(csv_reader) 
+
+        for row in csv_reader: 
+            data.append(dict(zip(headers, row))) 
+          
+    return data 
