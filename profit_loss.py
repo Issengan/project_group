00eeffd40f347c6fdb26abd396e79273ad2acaa6
avrialim <139242319+avrialim@users.noreply.github.com>
2023-07-29 23:26:10 +0800

    Update profit_loss.py : import CSV file, docstrings

--- profit_loss.py
+++ profit_loss.py
@@ -4,6 +4,12 @@ import csv
 def read_csv_file(file_path):
   """
   Read a CSV file and return the data as a list of dictionaries.
-  Parameters:
-  file_path(str):
+  Parameters: 
+        file_path (str): The path to the CSV file. 
+ 
+    Returns: 
+        list: A list of dictionaries containing the data from the CSV file. 
+    """ 
+
+  
   
