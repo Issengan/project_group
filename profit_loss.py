515220af9ef36572556a9a3591eb368f568d6aab
avrialim <139242319+avrialim@users.noreply.github.com>
2023-07-30 13:22:44 +0800

    Update profit_loss.py : obtaining surplus data

--- profit_loss.py
+++ profit_loss.py
@@ -47,4 +47,39 @@ def display_profit_loss_scenarios(profit_loss_file):
     # Initialize the output variable as an empty string 
     output = "" 
  
-
+ # Scenario 1: Check if net profit is positive for all days 
+    positive_net_profit = all(float(row['Net Profit']) >= 0 for row in profit_loss_data) 
+ 
+    # Check if net profit is positive for all days 
+    if positive_net_profit: 
+        # Append the relevant output to the output string if net profit is positive for all days 
+        output += "[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY\n" 
+        highest_surplus_day = None 
+        highest_surplus_amount = 0 
+ 
+        # Iterate through the profit_loss_data list starting from the second element (index 1) 
+        for i in range(1, len(profit_loss_data)): 
+            # Convert the 'Net Profit' values to float for comparison 
+            current_net_profit = float(profit_loss_data[i]['Net Profit']) 
+            previous_net_profit = float(profit_loss_data[i - 1]['Net Profit']) 
+ 
+            # Check if the current net profit is higher than the previous net profit 
+            if current_net_profit > previous_net_profit: 
+                # Calculate the net profit surplus for the current day 
+                net_profit_surplus = current_net_profit - previous_net_profit 
+ 
+                # Update the highest_surplus_day and highest_surplus_amount if needed 
+                if net_profit_surplus > highest_surplus_amount: 
+                    highest_surplus_day = i 
+                    highest_surplus_amount = net_profit_surplus 
+ 
+        # Check if there is a highest net profit surplus and append the relevant output 
+        if highest_surplus_day is not None: 
+            output += "[HIGHEST NET PROFIT SURPLUS] DAY: {}, AMOUNT: USD{}\n".format(highest_surplus_day, int(highest_surplus_amount)) 
+    else: 
+        # Iterate through the profit_loss_data list excluding the first and last elements 
+        for i in range(1, len(profit_loss_data) - 1): 
+            # Convert the 'Net Profit' values to float for comparison 
+            current_net_profit = float(profit_loss_data[i]['Net Profit']) 
+            previous_net_profit = float(profit_loss_data[i - 1]['Net Profit'])
+          
