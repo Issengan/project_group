b9e40df867810719056d1fb5e41fb76dbd051093
Issengan <140523844+Issengan@users.noreply.github.com>
2023-08-08 18:34:56 +0800

    Pushed new "main" function and cwd directory path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import necessary functions from respective modules
+from pathlib import Path
 from overheads import find_highest_overhead_percentage
 from cash_on_hand import display_cash_scenarios
 from profit_loss import display_profit_loss_scenarios
@@ -11,16 +12,18 @@ def main():
     3. Display profit and loss scenarios using data from 'Profits_and_Loss.csv' file.
     4. Write the outputs of all scenarios to the 'summary_report.txt' file.
     """
+    cwd = Path.cwd()  # Get the current working directory
+
     # Find highest overhead percentage
-    overheads_file = "../project_group/csv_reports/Overheads.csv"
+    overheads_file = cwd / "csv_reports" / "Overheads.csv"
     highest_overhead_output = find_highest_overhead_percentage(overheads_file)
 
     # Display cash scenarios
-    cash_on_hand_file = "../project_group/csv_reports/Cash_on_Hand.csv"
+    cash_on_hand_file = cwd / "csv_reports" / "Cash_on_Hand.csv"
     cash_output = display_cash_scenarios(cash_on_hand_file)
 
     # Display profit and loss scenarios
-    profit_loss_file = "../project_group/csv_reports/Profits_and_Loss.csv"
+    profit_loss_file = cwd / "csv_reports" / "Profits_and_Loss.csv"
     profit_loss_output = display_profit_loss_scenarios(profit_loss_file)
 
     with open("summary_report.txt", "w") as file:
@@ -28,9 +31,6 @@ def main():
         file.write(cash_output)
         file.write(profit_loss_output)
 
-    # The following block of code will only be executed if this script is run as the main program.
-    # This condition checks whether the script is being run directly and not imported as a module.
-    # Call the main function to execute the analysis and generate the summary report.
-    if __name__ == "__main__":
-        main()
+# Call the main function to execute the analysis and generate the summary report.
+main()
 
